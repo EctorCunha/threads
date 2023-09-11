bec03e3db4087ef754c17f7f6b8d85d01e4e00b2
Ector Cunha <ectorvccunha@outlook.com>
2023-09-11 20:21:56 -0300

    primeiro dia do Dev De Elite

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='bg-threads-bg-default text-threads-text'>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        threads: {
+          "bg-default": "#101010",
+          text: "#f3f5f7"
+        },
+      }
     },
   },
   plugins: [],
