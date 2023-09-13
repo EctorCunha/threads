84368af5709b4cef649c3776829c3106725e09b4
Ector Cunha <ectorvccunha@outlook.com>
2023-09-13 15:14:57 -0300

    add avatar, button and other components + post

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,55 +1,64 @@
-import Image from 'next/image'
-import threads from '@/assets/threads.svg'
-import {HomeIcon, Search, PenSquare, Heart, User} from 'lucide-react'
-import Link from 'next/link'
-import DropdownMenuHome from './dropdown'
+import Image from "next/image";
+import threads from "@/assets/threads.svg";
+import { HomeIcon, Search, PenSquare, Heart, User } from "lucide-react";
+import Link from "next/link";
+import DropdownMenuHome from "./dropdown";
+import HomePost from "./post/post";
 
 const menus = [
   {
     Logo: HomeIcon,
     alt: "Sua página inicial",
-    route: "/"
+    route: "/",
   },
   {
     Logo: Search,
     alt: "Pesquise coisas de seu interesse",
-    route: "/search"
+    route: "/search",
   },
   {
     Logo: PenSquare,
     alt: "O que você está pensando",
-    route: "/post"
+    route: "/post",
   },
   {
     Logo: Heart,
     alt: "Atividades",
-    route: "/activity"
+    route: "/activity",
   },
   {
     Logo: User,
     alt: "Seu perfil",
-    route: "/user"
+    route: "/user",
   },
-]
+];
 
 export default function Home() {
   return (
     <main>
-      <nav className='flex justify-between items-center py-1 px-6'>
-      <Image src={threads} height={32} alt='Logo do Threads'/>
-        <ul className='flex'>
-          {
-            menus.map((menu) => (
-              <li key={menu.alt} title={menu.alt}  className='px-7 py-5 hover:bg-neutral-800 rounded-xl transition-all duration-300 ease-in-out'>
-                <Link href={menu.route}>
-                  <menu.Logo size={28} className='text-neutral-600' />
-                </Link>
-              </li>
-            ))
-          }
+      <nav className="flex justify-between items-center py-1 px-6">
+        <Image src={threads} height={32} alt="Logo do Threads" />
+        <ul className="flex">
+          {menus.map((menu) => (
+            <li
+              key={menu.alt}
+              title={menu.alt}
+              className="px-7 py-5 hover:bg-neutral-800 rounded-xl transition-all duration-300 ease-in-out"
+            >
+              <Link href={menu.route}>
+                <menu.Logo size={28} className="text-neutral-600" />
+              </Link>
+            </li>
+          ))}
         </ul>
-        <DropdownMenuHome/>
+        <DropdownMenuHome />
       </nav>
+      <article className="max-w-lg m-auto">
+        <section>
+          <HomePost />
+        </section>
+        <hr className="mt-4 h-[0.5px] opacity-20" />
+      </article>
     </main>
-  )
+  );
 }
